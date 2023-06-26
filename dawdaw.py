da123ec5072d67cd0307c6ee1fb339e1ae02fb17
Ilya Kochegarov <bloodyberkut@gmail.com>
2023-06-27 06:31:10 +0700

    Done

--- dawdaw.py
+++ dawdaw.py
@@ -2,7 +2,7 @@ from random import randint, choice
 
 
 class Customer:
-    def __init__(self):
+    def __init__(self, lst):
         self.money = randint(15000, 50000)
         self.brand = choice(lst)
         print("I want to buy ", self.brand, "I have some money: ", self.money, "Can you recommend me smth?")
@@ -17,5 +17,13 @@ class Shop:
         self.brand5 = {"model1": 45000, "model2": 20000, "model3": 43000}
         self.brandlst = [self.brand1, self.brand2, self.brand3, self.brand4, self.brand5]
 
-    def recommend(self):
-        pass
+    def recommend(self, money, brand):
+        selected = [x for x in brand if money >= brand[x]]
+        if len(selected):
+            print("Sure, with your money, you can buy this models of chosen brand: ", *selected)
+        else:
+            print("Sorry, you cant buy any model of chosen brand :(")
+
+WatchShop = Shop()
+Bob = Customer(WatchShop.brandlst)
+WatchShop.recommend(Bob.money, Bob.brand)
