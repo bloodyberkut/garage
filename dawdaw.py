e946525cf007c915819c455ad83755cb6cf57174
Ilya Kochegarov <bloodyberkut@gmail.com>
2023-06-27 06:40:48 +0700

    added documentation

--- dawdaw.py
+++ dawdaw.py
@@ -1,15 +1,22 @@
+"""Модуль по задаче с екурсов"""
+
+
 from random import randint, choice
 
 
 class Customer:
+    """Класс описывающий покупателя"""
     def __init__(self, lst):
+        """Конструктор принимает список брэндов"""
         self.money = randint(15000, 50000)
         self.brand = choice(lst)
         print("I want to buy ", self.brand, "I have some money: ", self.money, "Can you recommend me smth?")
 
 
 class Shop:
+    """Класс описывающий магазин"""
     def __init__(self):
+        """Конструктор со словарями, содержащими наименование модели и цену"""
         self.brand1 = {"model1": 15000, "model2": 20000, "model3": 18000}
         self.brand2 = {"model1": 32000, "model2": 12000, "model3": 19000}
         self.brand3 = {"model1": 38000, "model2": 28000, "model3": 30000}
@@ -18,6 +25,7 @@ class Shop:
         self.brandlst = [self.brand1, self.brand2, self.brand3, self.brand4, self.brand5]
 
     def recommend(self, money, brand):
+        """Функция принимает два аргумента: int и dict. Выводит модели, цена которых лежит в пределах имеющихся денег"""
         selected = [x for x in brand if money >= brand[x]]
         if len(selected):
             print("Sure, with your money, you can buy this models of chosen brand: ", *selected)
